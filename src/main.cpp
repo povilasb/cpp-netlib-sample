071c349523f2e5c08c8efd4164206a29a8dba626
Povilas Balciunas <balciunas90@gmail.com>
2014-07-15 21:25:09 +0300

    Adds missing include, refactors code.

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,19 @@
 #include <iostream>
 
+#include <boost/network/protocol/http/client.hpp>
+
 
 using namespace std;
 using namespace boost::network;
-using namespace boost::network::http;
 
 
 int
 main(void)
 {
-	client::request request("http://povilasb.com");
+	http::client::request request("http://povilasb.com");
 	request << header("Connection", "close");
-	client client_;
-	client::response response_ = client_.get(request);
+	http::client client_;
+	http::client::response response = client_.get(request);
 	std::string body_ = body(response);
 
 	cout << body_ << '\n';
